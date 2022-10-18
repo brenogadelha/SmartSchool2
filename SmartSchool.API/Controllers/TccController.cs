a002bf299f67cf9a323c911df0435a626dc18c12
Breno Gadelha <breno_gadelha@hotmail.com>
2022-10-18 03:16:05 -0300

    Melhoria documentação.

--- SmartSchool.API/Controllers/TccController.cs
+++ SmartSchool.API/Controllers/TccController.cs
@@ -33,10 +33,10 @@ namespace SmartSchool.API.Controllers
 		}
 
 		/// <summary>
-		/// Obtem listagem de todos os Tccs cadastrados
+		/// Obtem listagem de todos os Temas de Tccs cadastrados
 		/// </summary>
-		/// <returns>Lista de todos os Tccs</returns>
-		/// <response code="200">Lista de Tccs</response> 
+		/// <returns>Lista de todos os Temas de Tccs</returns>
+		/// <response code="200">Lista de Temas de Tccs</response> 
 		/// <response code="500">Erro inesperado</response>
 		[ProducesResponseType(200, Type = typeof(IEnumerable<ObterTccsDto>))]
 		[ProducesResponseType(500, Type = typeof(TratamentoErroDto))]
@@ -49,11 +49,11 @@ namespace SmartSchool.API.Controllers
 		}
 
 		/// <summary>
-		/// Obtém dados do Tcc específico por ID
+		/// Obtém dados do Tema de Tcc específico por ID
 		/// </summary>
-		/// <returns>Dados do Tcc solicitado</returns>
-		/// <response code="200">Obtem dados do Tcc solicitado</response>
-		/// <response code="404">Tcc inexistente</response>
+		/// <returns>Dados do Tema de Tcc solicitado</returns>
+		/// <response code="200">Obtem dados do Tema de Tcc solicitado</response>
+		/// <response code="404">Tema de Tcc inexistente</response>
 		/// <response code="500">Erro inesperado</response>
 		[ProducesResponseType(200, Type = typeof(ObterTccDto))]
 		[ProducesResponseType(404, Type = typeof(TratamentoErroDto))]
@@ -67,10 +67,10 @@ namespace SmartSchool.API.Controllers
 		}
 
 		/// <summary>
-		/// Obtém lista de Solicitações de Tccs por Professor
+		/// Obtém lista de Solicitações de Temas de Tccs por Professor
 		/// </summary>
-		/// <returns>Lista de todos as solicitações de Tccs por Professor</returns>
-		/// <response code="200">Lista de solicitações de Tccs</response>
+		/// <returns>Lista de todos as solicitações de Temas de Tccs por Professor</returns>
+		/// <response code="200">Lista de solicitações de Temas de Tccs</response>
 		/// <response code="500">Erro inesperado</response>
 		[ProducesResponseType(200, Type = typeof(ObterSolicitacoesTccsDto))]
 		[ProducesResponseType(404, Type = typeof(TratamentoErroDto))]
@@ -84,11 +84,11 @@ namespace SmartSchool.API.Controllers
 		}
 
 		/// <summary>
-		/// Obtém Tcc solicitado por Aluno
+		/// Obtém Tema de Tcc solicitado por Aluno
 		/// </summary>
-		/// <returns>Dados do Tcc solicitado</returns>
-		/// <response code="200">Obtem dados do Tcc solicitado</response>
-		/// <response code="404">Tcc inexistente</response>
+		/// <returns>Dados do Tema de Tcc solicitado</returns>
+		/// <response code="200">Obtem dados do Tema de Tcc solicitado</response>
+		/// <response code="404">Tema de Tcc inexistente</response>
 		/// <response code="500">Erro inesperado</response>
 		[ProducesResponseType(200, Type = typeof(ObterSolicitacoesTccsDto))]
 		[ProducesResponseType(404, Type = typeof(TratamentoErroDto))]
@@ -102,11 +102,11 @@ namespace SmartSchool.API.Controllers
 		}
 
 		/// <summary>
-		/// Cria um novo Tcc
+		/// Cria um novo Tema para o Tcc
 		/// </summary>
 		/// <returns>Http status 201(Created)</returns>
-		/// <response code="200">Tcc criado com sucesso</response>
-		/// <response code="400">Dados inconsistentes para criação do Tcc</response>
+		/// <response code="200">Tema para o Tcc criado com sucesso</response>
+		/// <response code="400">Dados inconsistentes para criação do Tema de Tcc</response>
 		/// <response code="422">Erro de Negócio</response>
 		/// <response code="500">Erro inesperado</response> 
 		[HttpPost]
@@ -122,12 +122,12 @@ namespace SmartSchool.API.Controllers
 		}
 
 		/// <summary>
-		/// Efetua alteração de Tcc
+		/// Efetua alteração de Tema para o Tcc
 		/// </summary>
 		/// <returns>Http status 204(No Content)</returns>
-		/// <response code="204">Tcc alterado com Sucesso</response>
-		/// <response code="400">Dados para alteração de Tcc inconsistentes.</response>
-		/// <response code="404">Tcc inexistente</response>
+		/// <response code="204">Tema de Tcc alterado com Sucesso</response>
+		/// <response code="400">Dados para alteração de Tema do Tcc inconsistentes.</response>
+		/// <response code="404">Tema de Tcc inexistente</response>
 		/// <response code="422">Erro de Negócio</response>
 		/// <response code="500">Erro inesperado</response> 
 		[HttpPut("{id}")]
@@ -152,13 +152,13 @@ namespace SmartSchool.API.Controllers
 		}
 
 		/// <summary>
-		/// Aprova Tcc solicitado pelo Aluno
+		/// Aprova Tema de Tcc solicitado pelo Aluno
 		/// </summary>
 		/// <returns>Http status 204(No Content)</returns>
-		/// <response code="204">Tcc aprovado com Sucesso</response>
+		/// <response code="204">Tema para o Tcc aprovado com Sucesso</response>
 		/// <response code="400">Dados para aprovação de Tcc inconsistentes.</response>
 		/// <response code="401">Não autorizado</response>
-		/// <response code="404">Tcc inexistente</response>
+		/// <response code="404">Tema de Tcc inexistente</response>
 		/// <response code="422">Erro de negócio.</response>
 		/// <response code="500">Erro inesperado</response> 
 		[HttpPut("{professor-id}/professores/{aluno-id}/alunos/aprovar")]
@@ -175,11 +175,11 @@ namespace SmartSchool.API.Controllers
 		}
 
 		/// <summary>
-		/// Exclui um Tcc específico
+		/// Exclui um Tema de Tcc específico
 		/// </summary>
-		/// <response code="204">Tcc excluído com Sucesso</response>
+		/// <response code="204">Tema de Tcc excluído com Sucesso</response>
 		/// <response code="400">Dados para exclusão do Tcc inconsistentes.</response>
-		/// <response code="404">Tcc inexistente</response>
+		/// <response code="404">Tema de Tcc inexistente</response>
 		/// <response code="500">Erro inesperado</response> 
 		[HttpDelete("{id}")]
 		[ProducesResponseType(204)]
@@ -193,11 +193,11 @@ namespace SmartSchool.API.Controllers
 		}
 
 		/// <summary>
-		/// Solicita Tcc
+		/// Solicita Tema para o Tcc
 		/// </summary>
 		/// <returns>Http status 204(No Content)</returns>
-		/// <response code="204">Tcc solicitado com Sucesso</response>
-		/// <response code="400">Dados para solicitação de Tcc inconsistentes.</response>
+		/// <response code="204">Tema para o Tcc solicitado com Sucesso</response>
+		/// <response code="400">Dados para solicitação de Tema para o Tcc inconsistentes.</response>
 		/// <response code="401">Não autorizado</response>
 		/// <response code="404">Tcc inexistente</response>
 		/// <response code="422">Erro de negócio.</response>
